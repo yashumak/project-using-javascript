15f7d349a545d73dc6e1c0dc4470a1925c0266df
YASH UMAK <patil.rtx@gmail.com>
2025-01-17 20:07:24 +0530

    Update chatbot.js

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 let userText = null;
 const API_KEY =
-  "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";
+  "sk-proj-4mc.....";
 
 const loadDataFromLocalstorage = () => {
   const themeColor = localStorage.getItem("themeColor");
